a85220f409125f6cd2f3153e234a6f7145f89692
nicolasfranco777 <nf702136@gmail.com>
2025-09-22 22:04:23 -0500

    Update and rename README.md to BusquedaHashj.java

---
+++ BusquedaHashj.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class BusquedaHashj {
+    public static void main(String[] args) {
+        
+        HashMap<Integer, String> mapa = new HashMap<>();
+
+        mapa.put(1, "Perú");
+        mapa.put(2, "Argentina");
+        mapa.put(3, "Brasil");
+        mapa.put(4, "Chile");
+
+        int claveBuscada = 6;
+        if (mapa.containsKey(claveBuscada)) {
+            System.out.println("Encontrado: clave " + claveBuscada + " -> " + mapa.get(claveBuscada));
+        } else {
+            System.out.println("Clave " + claveBuscada + " no encontrada.");
+        }
+
+        String paisBuscado = "Bolivia";
+        if (mapa.containsValue(paisBuscado)) {
+            System.out.println("El valor '" + paisBuscado + "' está en la tabla hash.");
+        } else {
+            System.out.println("El valor '" + paisBuscado + "' no está en la tabla hash.");
+        }
+    }
+}
